02215772b15142e5fd488c0663fe1a70a68dba43
Neven Hajder <neven.hajder@gmail.com>
2016-01-26 13:16:55 -0500

    Udacity instruction complete

--- js/helper.js
+++ js/helper.js
@@ -1,27 +1,17 @@
 /*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
+These are HTML strings.
+In resumeBuilder.js functions will replace "%data%" with meaningful information.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="role">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><a href="mailto:neven.hajder@gmail.com"><img class="icon envelope" src="images/envelope.png" title="Send an email." alt="Envelope"><a></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><a href="https://github.com/nevenhajder"><img class="icon github" src="images/github-logo.png" title="Check out my GitHub page." alt="GitHub Logo"></a></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><img class="icon location" src="images/location-icon.png" title="Where I reside." alt="A location pin."></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
@@ -55,52 +45,17 @@ var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
-
 /*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
-  });
-});
-
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
-clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-  // your code goes here!
-});
-
-
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
+Generate the custom Google Map for the website.
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
 var map;    // declares a global map variable
 
 
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
+/* initializeMap() is called when page is loaded. */
 function initializeMap() {
 
   var locations;
@@ -109,9 +64,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -129,21 +84,16 @@ function initializeMap() {
     locations.push(bio.contacts.location);
 
     // iterates through school locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    // the locations array.
     education.schools.forEach(function(school){
       locations.push(school.location);
     });
 
     // iterates through work locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    // the locations array.
     work.jobs.forEach(function(job){
       locations.push(job.location);
     });
-
     return locations;
   }
 
@@ -174,9 +124,9 @@ function initializeMap() {
       content: name
     });
 
-    // hmmmm, I wonder what this is about...
+    /* Open the infoWindow when the user clicks on the marker */
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -209,7 +159,7 @@ function initializeMap() {
     var service = new google.maps.places.PlacesService(map);
 
     // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
+    locations.forEach(function(place){
       // the search request object
       var request = {
         query: place
@@ -233,16 +183,5 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
-
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
-
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('load', initializeMap);
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,223 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+/* Objects holding information to be inserted into the HTML */
+
+var bio = {
+    "name": "Neven Hajder",
+    "role": "(Aspiring) Web Developer",
+    "contacts": {
+        "email": "neven.hajder@gmail.com",
+        "githubURL": "https://github.com/nevenhajder",
+        "location": "Kitchener, Ontario"
+    },
+    "bioPicURL": "http://www.washingtoncitypaper.com/blogs/youngandhungry/files/2008/11/benjamin-franklin.jpg",
+    "welcomeMessage": "Welcome to my humble website.",
+    "skills": [
+        "Skill1",
+        "Skill2",
+        "Skill3",
+        "Skill4",
+        "Skill5"
+    ],
+    /* Display all of the bio info */
+    display: function() {
+        /* Insert info into generic variables from helper.js */
+        var formattedName = HTMLheaderName.replace('%data%', bio.name);
+        var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+        var formattedBioPic = HTMLbioPic.replace('%data%', bio.bioPicURL);
+        var formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+
+        var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
+        var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.githubURL);
+        var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+
+        function formatSkills() {
+            /* Get skills from the bio object */
+            var skills = bio.skills;
+
+            /* Check that there are skills */
+            if (skills.length > 0) {
+                var formattedSkills = [];
+                var newSkill = '';
+
+                /* Insert info into generic varibales from helper.js */
+                skills.forEach(function(entry,index) {
+                    newSkill = HTMLskills.replace('%data%', bio.skills[index]);
+                    formattedSkills.push(newSkill);
+                });
+
+                return formattedSkills;
+            }
+        }
+
+        /* Format and store the skills as HTML */
+        var formattedSkills = formatSkills();
+
+        /* Attach Bio info to the HTML (order of appending/prepending is important) */
+        /* Header */
+        $('#header').append(formattedBioPic);
+        $('#header').append(formattedWelcome);
+
+        /* If there is more than one skill then create variables and attach them to HTML */
+        if (formattedSkills.length > 0) {
+            $('#header').append(HTMLskillsStart);
+
+            formattedSkills.forEach(function(entry){
+                $('#skills').append(entry);
+            });
+        }
+
+        $('#header').prepend(formattedName);
+        $('#name').append(formattedRole);
+
+        /* Contact */
+        $('#footerContacts').append(formattedEmail);
+        $('#footerContacts').append(formattedGithub);
+        $('#footerContacts').append(formattedLocation);
+    }
+};
+
+var work = {
+    "jobs": [
+        {
+            "title": "Unemployed",
+            "employer": "Mom",
+            "dates": "January 2016 - present",
+            "location": "Kitchener",
+            "description": ""
+        },
+        {
+            "title": "English Teacher",
+            "employer": "Seosanyecheon Elementary School",
+            "dates": "January 2014 - January 2016",
+            "location": "Seosan, South Korea",
+            "description": ""
+        },
+        {
+            "title": "English Teacher",
+            "employer": "ELA Language Academy",
+            "dates": "October 2012 - October 2013",
+            "location": "Daegu",
+            "description": ""
+        }
+    ],
+    display: function() {
+        /* Create and disply a new .work-entry for each job */
+        work.jobs.forEach(function(entry,index) {
+            /* Insert info into generic variables from helper.js */
+            var employer = HTMLworkEmployer.replace('%data%', work.jobs[index].employer);
+            var title = HTMLworkTitle.replace('%data%', work.jobs[index].title);
+            var date = HTMLworkDates.replace('%data%', work.jobs[index].dates);
+            var location = HTMLworkLocation.replace('%data%', work.jobs[index].location);
+
+            /* Create new .work-entry */
+            $('#workExperience').append(HTMLworkStart);
+            /* Append elements to the new .work-entry */
+            $('.work-entry:last').append(employer + title);
+            $('.work-entry:last').append(date);
+            $('.work-entry:last').append(location);
+        });
+    }
+};
+
+var projects = {
+    "projects": [
+    {
+        "title": "Abacus",
+        "description": "",
+        "img": "",
+        "tags": ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        "title": "Library",
+        "description": "",
+        "img": "",
+        "tags": ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        "title": "Game",
+        "description": "",
+        "img": "",
+        "tags": ["HTML", "CSS", "jQuery"]
+    }
+    ],
+    display: function() {
+        /* Fix for function invocation context problems with "this" */
+        var me = this;
+
+        me.projects.forEach(function(entry,index) {
+            /* Create variables with HTML templates */
+            var title = HTMLprojectTitle.replace("%data%", me.projects[index].title);
+            var description = HTMLprojectDescription.replace("%data%", me.projects[index].description);
+            var img = HTMLprojectImage.replace("%data%", me.projects[index].img);
+            /* Create a new .project-entry */
+            $("#projects").append(HTMLprojectStart);
+            /* Append project information to .project-entry */
+            $(".project-entry:last").append(title);
+            $(".project-entry:last").append(description);
+            $(".project-entry:last").append(img);
+        });
+    }
+};
+
+var education = {
+    "schools": [
+        {
+            "schoolName": "The World Wide Web",
+            "degree": "Front End Web Developer",
+            "dates": "January 2014 - present",
+            "location": "San Francisco"
+        },
+        {
+            "schoolName": "University of Waterloo",
+            "degree": "BSc. - Kinesiology",
+            "dates": "January 2008 - May 2012",
+            "location": "Waterloo, Ontario, Canada"
+        }
+    ],
+    display: function() {
+        var me = this;
+
+        me.schools.forEach(function(entry,index) {
+            /* Insert info into generic variables from helper.js */
+            var name = HTMLschoolName.replace('%data%', me.schools[index].schoolName);
+            var degree = HTMLschoolDegree.replace('%data%', me.schools[index].degree);
+            var date = HTMLschoolDates.replace('%data%', me.schools[index].dates);
+            var location = HTMLschoolLocation.replace('%data%', me.schools[index].location);
+
+            /* Create new .education-entry */
+            $('#education').append(HTMLschoolStart);
+            /* Append elements to the new .education-entry */
+            $('.education-entry:last').append(name + degree);
+            $('.education-entry:last').append(date);
+            $('.education-entry:last').append(location);
+        });
+    }
+};
+
+
+/* Insert information into the HTML */
+
+/* Bio */
+bio.display();
+
+/* Work */
+work.display();
+
+/* Projects */
+projects.display();
+
+/* Education */
+education.display();
+
+
+/* Google Map */
+/* Append googleMap to the #mapDiv in the HTML */
+$("#mapDiv").append(googleMap);
+
+/* TODO:
+    Incorporate a JavaScript library, like D3, to graph or chart your skills
+*/
+
+
+
+
